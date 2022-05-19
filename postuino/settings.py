ef0f00e08cd4cab2fc8efa0dfc25636d737cd088
hkatkade <hkatkade7@gmail.com>
2022-05-19 23:06:57 +0530

    postgres

--- postuino/settings.py
+++ postuino/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.herokuapp.com','postuinooo.herokuapp.com/']
 
 
 # Application definition
