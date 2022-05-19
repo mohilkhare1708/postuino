bac871d080929150ba74b1e78b4a22cba0780c81
hkatkade <hkatkade7@gmail.com>
2022-05-19 23:22:49 +0530

    postgres

--- postuino/urls.py
+++ postuino/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.conf import settings
-from django.urls import path, include
+from django.urls import path, include, re_path
 from mainapp import views as mainapp_views
 from django.views.static import serve
 from django.conf.urls import url
@@ -24,6 +24,6 @@ from django.conf.urls import url
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('mainapp.urls')),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':        settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root':        settings.MEDIA_ROOT}), 
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
